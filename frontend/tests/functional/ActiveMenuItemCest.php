10ad1ee34c9db1da12f93f2c7de54c33993aeec0
Sergeygeek <sukhorukov1986@yandex.ru>
2019-02-20 20:35:39 +0400

    Functional test corrected

--- frontend/tests/functional/ActiveMenuItemCest.php
+++ frontend/tests/functional/ActiveMenuItemCest.php
@@ -1,24 +1,35 @@
 <?php namespace frontend\tests\functional;
+use Codeception\Codecept;
+use Codeception\Example;
 use frontend\tests\FunctionalTester;
+use yii\helpers\VarDumper;
 
 class ActiveMenuItemCest
 {
     public function _before(FunctionalTester $I)
     {
-        $I->amOnRoute('site/index');
     }
 
-    // tests
-    public function tryToTest(FunctionalTester $I)
+    /**
+     * @dataProvider pageProvider
+     */
+    public function tryToTest(FunctionalTester $I, \Codeception\Example $pages)
     {
-        $I->see('Home', '.active');
-        $I->click('About');
-        $I->see('About', '.active');
-        $I->click('Contact');
-        $I->see('Contact', '.active');
-        $I->click('Signup');
-        $I->see('Signup', '.active');
-        $I->click('Login');
-        $I->see('Login', '.active');
+        $I->amOnPage($pages['url']);
+        $I->see($pages['link'], '.active');
+    }
+
+    /**
+     * @return array
+     */
+    protected function pageProvider()
+    {
+        return [
+            ['url'=>"site/index", 'title'=>"My Yii Application", 'link'=>"Home"],
+            ['url'=>"site/about", 'title'=>"About", 'link'=>"About"],
+            ['url'=>"site/contact", 'title'=>"Contact", 'link'=>"Contact"],
+            ['url'=>"site/signup", 'title'=>"Signup", 'link'=>"Signup"],
+            ['url'=>"site/login", 'title'=>"Login", 'link'=>"Login"],
+        ];
     }
 }
